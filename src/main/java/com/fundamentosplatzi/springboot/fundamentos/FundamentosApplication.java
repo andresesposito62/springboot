dd2ee0a2722447cdc2f4d506e5412016dcab2cce
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2022-09-11 15:48:27 -0500

    Bean implementation

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,23 +1,27 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner{
 
 private ComponentDependency componentDependency;
+private MyBean myBean;
 
 
 //Cuando tenemos multiples implementaciones de una
 //dependencia, se usa la etiqueta Qualifier para indicar
 //cual dependencia se desea inyectar
-public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+public FundamentosApplication(MyBean myBeanInyected, @Qualifier("componentTwoImplement") ComponentDependency componentDependency){
 	this.componentDependency = componentDependency;
+	this.myBean = myBeanInyected;
 }
 
 	public static void main(String[] args) {
@@ -26,8 +30,8 @@ public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDepen
 
 	@Override
 	public void run(String... args) throws Exception {
-		componentDependency.saludar();
-		
+		//componentDependency.saludar();
+		myBean.print();
 	}
 
 }
