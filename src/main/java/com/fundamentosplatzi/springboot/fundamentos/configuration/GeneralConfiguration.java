4ef314256a1faf7cf3a061078e63ff4125f69d24
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2022-09-21 21:01:03 -0500

    Uso de anotacion value para apuntar a properties

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -7,11 +7,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}") //permite llamar a valores que hemos incializado en application.properties
@@ -22,6 +25,18 @@ public class GeneralConfiguration {
 
     @Value("${value.random}") //permite llamar a valores que hemos incializado en application.properties
     private String random;
+    
+    @Value("${jdbc.url}")
+    private String jdbcurl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
 
     @Bean
     public MyBeanWithProperties function(){
@@ -31,10 +46,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         return DataSourceBuilder.create()
-                .driverClassName("org.h2.Driver")
-                    .url("jdbc:h2:mem:testdb")
-                     .username("sa")
-                      .password("")
+                .driverClassName(driver)
+                    .url(jdbcurl)
+                     .username(username)
+                      .password(password)
                        .build();
         }
 
